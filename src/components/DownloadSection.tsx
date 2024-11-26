82a229d4a7cdb16a02e79e0959a8f0623d47b510
Anmol <anmolpaweriya0235@gmail.com>
2024-11-26 17:03:45 +0530

    fix eslint issues

--- src/components/DownloadSection.tsx
+++ src/components/DownloadSection.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { z } from 'zod'
 import { useForm } from 'react-hook-form'
 import { Button } from "@/components/ui/button"
